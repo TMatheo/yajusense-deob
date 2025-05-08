6de5cae94e9c13de0f59f256d61bcd3a7cb39eab
tadon1203 <nosashuumaiepic@gmail.com>
2025-05-09 00:39:19 +0900

    wow

---
+++ yajusense/src/Patches/SetRequestHeaderPatch.cs
@@ -0,0 +1,14 @@
+// using HarmonyLib;
+// using UnityEngine.Networking;
+//
+// namespace yajusense.Patches;
+//
+// [HarmonyPatch(typeof(UnityWebRequest), nameof(UnityWebRequest.SetRequestHeader))]
+// public static class SetRequestHeaderPatch
+// {
+// 	[HarmonyPostfix]
+// 	public static void Postfix(string name, string value)
+// 	{
+// 		Plugin.Log.LogInfo($"SetRequestHeader: {name} {value}");
+// 	}
+// }
