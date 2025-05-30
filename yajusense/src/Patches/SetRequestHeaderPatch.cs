a622b39bba362855d9808e7f9630ac998dae4090
LXQtie <111549440+TMatheo@users.noreply.github.com>
2025-05-30 17:55:06 +0200

    war

--- yajusense/src/Patches/SetRequestHeaderPatch.cs
+++ yajusense/src/Patches/SetRequestHeaderPatch.cs
@@ -1,15 +1,15 @@
-// using HarmonyLib;
-// using UnityEngine.Networking;
-//
-// namespace yajusense.Patches;
-//
-// [HarmonyPatch(typeof(UnityWebRequest), nameof(UnityWebRequest.SetRequestHeader))]
-// public static class SetRequestHeaderPatch
-// {
-// 	[HarmonyPostfix]
-// 	public static void Postfix(string name, string value)
-// 	{
-// 		Plugin.Log.LogInfo($"SetRequestHeader: {name} {value}");
-// 	}
-// }
+using HarmonyLib;
+using UnityEngine.Networking;
+
+ namespace yajusense.Patches;
+
+ [HarmonyPatch(typeof(UnityWebRequest), nameof(UnityWebRequest.SetRequestHeader))]
+ public static class SetRequestHeaderPatch
+ {
+ 	[HarmonyPostfix]
+ 	public static void Postfix(string name, string value)
+ 	{
+ 		Plugin.Log.LogInfo($"SetRequestHeader: {name} {value}");
+ 	}
+}
 
