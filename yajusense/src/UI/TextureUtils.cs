d0454180383736f7601e9da15a02015a8ae62a63
LXQtie <111549440+TMatheo@users.noreply.github.com>
2025-05-30 20:52:22 +0200

    yep

---
+++ yajusense/src/UI/TextureUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace yajusense.src.UI
+{
+    public static class TextureUtils
+    {
+        public static Texture2D MakeColorTexture(Color color)
+        {
+            Texture2D tex = new Texture2D(1, 1);
+            tex.SetPixel(0, 0, color);
+            tex.Apply();
+            return tex;
+        }
+    }
+}
